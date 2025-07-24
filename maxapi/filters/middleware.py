fd048e85447c5fa3ce944632bf9cd3814bf5cd8a
Denis <bestloveapples@gmail.com>
2025-07-25 00:53:08 +0300

    ignore

--- maxapi/filters/middleware.py
+++ maxapi/filters/middleware.py
@@ -19,9 +19,9 @@ class BaseMiddleware:
         kwargs_temp = {'data': result_data_kwargs.copy()}
         
         for key in kwargs_temp.copy().keys():
-            if not key in self.__call__.__annotations__.keys():
+            if key not in self.__call__.__annotations__.keys(): # type: ignore
                 del kwargs_temp[key]
         
-        result: Dict[str, Any] = await self(event_object, **kwargs_temp)
+        result: Dict[str, Any] = await self(event_object, **kwargs_temp) # type: ignore
         
         return result
